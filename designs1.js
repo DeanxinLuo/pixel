c82ce7b05049faf80523099b9002f8fdd5e3e6dd
DeanxinLuo <88123885+DeanxinLuo@users.noreply.github.com>
2021-08-17 11:59:21 +1000

    Add files via upload

--- designs1.js
+++ designs1.js
@@ -4,29 +4,33 @@
 // When size is submitted by the user, call makeGrid()
 
 // Your code goes here!
-const userColor = document.getElementById('colorPicker').value;
+const userColor = document.getElementById('colorPicker');
 const theTable = document.getElementById('pixelCanvas');
 const theHeight = document.getElementById('inputHeight').value;
 const theWidth = document.getElementById('inputWidth').value;
 const size = document.getElementById('sizePicker');
-function makeGrid(theHeight, theWidth) {
-    // Creating the grid by the user
-    for (const i = 0; i < theHight; i++) {
-        const row = theTable.insertRow(i);
-        for (const x = 0; x < theWidth; x++) {
-            row.insertCell(x);
-        }
-    }
-};
+
 // when user make grid, the correspond happend
-size.addEventListener('submit', function (evt) {
-    evt.preventDefault();
-    makeGrid();
-});
+size.addEventListener('submit', function (a) {
+    a.preventDefault();
+    theTable.firstElementChild.remove();
+    makeGrid(theHeight, theWidth);
+  });
+
 // when user choose color, the respond occured
-theTable.addEventListener('click', function (evt) {
-    if (evt.target.nodeName.toLowerCase() === 'td') {
-        evt.target.style.backgroundColor = theColor;
-    }
+cell.addEventListener('click',function(a) {
+  if (a.target.nodeName.toLowerCase() === 'td') {
+    a.target.style.backgroundColor = userColor.value;
+  }
 });
+
+// Creating the grid by the user
+function makeGrid(theHeight, theWidth) {
+  theTable.innerHTML = '';
+  for (const i = 0; i < theHeight; i += 1) {
+    const row = theTable.insertRow(i);
+    for (const x = 0; x < theWidth; x += 1) {
+      const cell = row.insertCell(x);
+  }
+ }
 }
